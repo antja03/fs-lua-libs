c27d39d2d436ff67d0af5ba35ce680e93c5669dc
antja03 <antja.03@gmail.com>
2020-12-03 19:49:07 -0500

    Cleared main function

--- script.lua
+++ script.lua
@@ -1,21 +1,5 @@
 local CMinecraft = require("sdk1710.client.CMinecraft")
 
 function main()
-    local minecraft = CMinecraft:new()
 
-    local player = minecraft:getPlayers()
-    local x, y, z = table.unpack(player:getPosition())
-    cout(tostring(x))
-    cout(tostring(y))
-    cout(tostring(z))
-
-    local world = minecraft:getWorld()
-    local playerList = world:getPlayers()
-
-    for idx, ply in pairs(playerList) do
-        local plyX, plyY, plyZ = table.unpack(ply:getPosition())
-        cout(tostring(plyX))
-        cout(tostring(plyY))
-        cout(tostring(plyZ))
-    end
 end
